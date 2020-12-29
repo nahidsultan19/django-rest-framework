0aab8095675c4ac7221aa9a442c0d290adc1e79f
nahidsultan19 <nahidsultan38@gmail.com>
2020-12-29 18:08:19 +0600

    add viewset class base

--- ClassBasedRest/urls.py
+++ ClassBasedRest/urls.py
@@ -1,12 +1,26 @@
 from django.urls import path
-from .views import BookApiView, BookApiDetail
-# from . import views
-from rest_framework.urlpatterns import format_suffix_patterns
+# from .views import BookApiView, BookApiDetail
+# # from . import views
+# from rest_framework.urlpatterns import format_suffix_patterns
+
+# urlpatterns = [
+#     path('', BookApiView.as_view()),
+#     path('detail/<str:pk>/', BookApiDetail.as_view())
+#     # path('', views.BookApiView)
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+from .views import BookViewSet
 
 urlpatterns = [
-    path('', BookApiView.as_view()),
-    path('detail/<str:pk>/', BookApiDetail.as_view())
-    # path('', views.BookApiView)
+    path('', BookViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('books/<str:pk>', BookViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
